c6621c4d7d8c4a5b892c326eea664a71d40f1247
tesikaasemi2003 <asemitesika68@gmail.com>
2026-03-26 01:17:05 +0530

    commit message

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const main = document.getElementById('main');
 const vertical = document.getElementById('vertical');
 const tagline = document.getElementById('tagline');
 
+person.addEventListener('mouseenter', () => {
+    hero.classList.add('person-hover-effect');
+
+    logo.innerHTML = `UI/UX<br>DESIGNER`;
+    main.textContent = `DESIGNER`;
+    vertical.textContent = `UI/UX DESIGNER`;
+    tagline.textContent = `That's Me again!`;
+});
