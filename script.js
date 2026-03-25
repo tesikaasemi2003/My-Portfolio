51775b1f7a773e287a4588377b294314590f2b5b
tesikaasemi2003 <asemitesika68@gmail.com>
2026-03-25 22:03:49 +0530

    commit message

---
+++ script.js
@@ -0,0 +1,6 @@
+const hero = document.getElementById('hero');
+const person = document.getElementById('person');
+const logo = document.getElementById('logo');
+const main = document.getElementById('main');
+const vertical = document.getElementById('vertical');
+const tagline = document.getElementById('tagline');
