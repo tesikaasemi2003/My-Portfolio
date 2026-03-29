06367684fda370fea3b905c9ce6c59b01cac5d5d
tesikaasemi2003 <asemitesika68@gmail.com>
2026-03-29 19:43:43 +0530

    commit message

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-const hero = document.getElementById('hero');
 const person = document.getElementById('person');
-const logo = document.getElementById('logo');
-const main = document.getElementById('main');
+const main   = document.getElementById('main');
 const vertical = document.getElementById('vertical');
-const tagline = document.getElementById('tagline');
+const tagline  = document.getElementById('tagline');
 
 person.addEventListener('mouseenter', () => {
-    hero.classList.add('person-hover-effect');
-
-    logo.innerHTML = `SOFTWARE<br>ENGINEER`;
-    main.textContent = `ENGINEER`;
-    vertical.textContent = `SOFTWARE ENGINEER`;
-    tagline.textContent = `That's Me again!`;
+    main.textContent     = 'ENGINEER';
+    vertical.textContent = 'SOFTWARE ENGINEER';
+    tagline.textContent  = "That's Me again!";
 });
 
 person.addEventListener('mouseleave', () => {
-    hero.classList.remove('person-hover-effect');
-
-    logo.innerHTML = `UI/UX<br>DESIGNER`;
-    main.textContent = `DESIGNER`;
-    vertical.textContent = `UI/UX DESIGNER`;
-    tagline.textContent = `Yes, that's Me!`;
+    main.textContent     = 'DESIGNER';
+    vertical.textContent = 'UI/UX DESIGNER';
+    tagline.textContent  = "Yes, that's Me!";
 });
